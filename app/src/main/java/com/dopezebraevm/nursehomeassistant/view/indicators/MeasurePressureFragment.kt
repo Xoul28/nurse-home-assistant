4555a543594edd58e5c073de9655b669a6f0a023
Evgeny Mezentsev <eimezentsev@yandex.ru>
2020-05-04 04:36:10 +0300

    Added measure pressure screen

---
+++ app/src/main/java/com/dopezebraevm/nursehomeassistant/view/indicators/MeasurePressureFragment.kt
@@ -0,0 +1,10 @@
+package com.dopezebraevm.nursehomeassistant.view.indicators
+
+import com.dopezebraevm.nursehomeassistant.BaseFragment
+import com.dopezebraevm.nursehomeassistant.R
+
+/**
+ * Created by Evgeniy Mezentsev on 04.05.2020.
+ */
+class MeasurePressureFragment : BaseFragment(R.layout.fragment_measure_pressure) {
+}
